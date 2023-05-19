f9eea1c7684a6789731d13944b28f556ef9b5b75
Alessio Rizzolo <aless.rizzolo@gmail.com>
2023-05-19 10:33:34 +0200

    Added if condition

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+image=$(docker run -d --rm -p 4566:4566 --name aws localstack/localstack)
+
+#if the container is not running exit 
+if [ -z "$image" ];
+then
+    echo "Failed to create a container"
+    exit 1
+fi
