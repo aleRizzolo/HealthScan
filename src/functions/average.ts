86d7945a63dace66ffccfe0f2c368422d31630c7
Alessio Rizzolo <aless.rizzolo@gmail.com>
2023-05-28 15:42:37 +0200

    Modified device script

--- src/functions/average.ts
+++ src/functions/average.ts
@@ -1,19 +1,23 @@
+import { APIGatewayProxyEvent } from "aws-lambda"
+import { PutItemCommand } from "@aws-sdk/client-dynamodb"
 import { ReceiveMessageCommand, DeleteMessageCommand } from "@aws-sdk/client-sqs"
 
-import { queueClient } from "../index"
+import { ddbClient, queueClient } from "../index"
 
 const BEACHES_QUEUE = ["long_beach", "venice_beach", "santa_monica_beach", "manhattan_beach"]
 
 const SQS_QUEUE_URL = process.env.ENDPOINT + "/000000000000/"
 
-const recieve = async () => {
-  let averagePH = 0
+export const lambdaHandler = async () => {
   for (let queue = 0; queue < BEACHES_QUEUE.length; queue++) {
+    let messageCount = 0
+    let finalAverage = 0
+
     try {
       const receiveMessageParams = {
         QueueUrl: SQS_QUEUE_URL + BEACHES_QUEUE[queue],
         MaxNumberOfMessages: 10, // Max number of messages to read
-        WaitTimeSeconds: 20, // Max waiting time for messages (max value 20 secs)
+        WaitTimeSeconds: 5, // Max waiting time for messages (max value 20 secs)
       }
 
       const command = new ReceiveMessageCommand(receiveMessageParams)
@@ -22,26 +26,51 @@ const recieve = async () => {
 
       if (messages && messages.length > 0) {
         for (const message of messages) {
-          const body = JSON.parse(message.Body!)
-          console.info("SQS message received")
-          averagePH += parseInt(body.ph.split(" ")[0])
+          messageCount++
+          var body = JSON.parse(message.Body!)
+          //console.info("SQS message received")
+          finalAverage += parseInt(body.ph.split(" ")[0])
+          /*console.log("Body message:", body)
+          console.log("Average intermediate:", finalAverage)*/
 
-          /*await queueClient.send(
+          await queueClient.send(
             new DeleteMessageCommand({
-              QueueUrl: SQS_QUEUE_URL + POLLUTION_QUEUE_NAMES[queue],
+              QueueUrl: SQS_QUEUE_URL + BEACHES_QUEUE[queue],
               ReceiptHandle: message.ReceiptHandle,
             })
           )
-          console.info("Message eliminated")*/
+          //console.info("Message eliminated")
         }
+      }
+      finalAverage = parseInt((finalAverage / messageCount).toFixed(2))
+      console.log("Average for", BEACHES_QUEUE[queue], ":", finalAverage)
+
+      //Save into DynamoDB
+      const commandDB = new PutItemCommand({
+        TableName: "SeaScan",
+        Item: {
+          zone: { S: BEACHES_QUEUE[queue] },
+          ph: { S: finalAverage.toString() },
+          timeStamp: { S: body.timeStamp },
+          dayTime: { S: body.dayTime },
+        },
+      })
+      const responseDB = await ddbClient.send(commandDB)
+
+      if (!responseDB) {
       } else {
-        console.info("No message in queue")
+        console.info("Database populated")
       }
     } catch (error) {
-      console.error("Error during SQS reading", error)
+      console.log("Error reading from queue", error)
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: "some error happened",
+        }),
+      }
     }
   }
-  averagePH = averagePH / 4
 }
 
-recieve()
+lambdaHandler()
